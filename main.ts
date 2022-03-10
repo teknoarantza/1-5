26b8b2389920a506940877e0911b340cd779857e
teknoarantza <99352288+teknoarantza@users.noreply.github.com>
2022-03-10 19:47:29 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.South)
+        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.NorthWest)
+        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.South)
+        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.NorthEast)
+    }
+})
